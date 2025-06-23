c626550172a03baa8583b0d61149e8a5bd6cd3ce
igor2077-programador <igorminlpr@gmail.com>
2025-06-23 17:30:17 -0300

    git push

---
+++ jogo da velha/script.js
@@ -0,0 +1,12 @@
+var jogador="x"
+
+function jogar(celula){
+    if(celula.innerHTML =="")
+        celula.innerHTML = jogador;
+    if(jogador =="x"){
+        jogador = "o"
+    } 
+    else{
+        jogador = "x"
+    }
+}
